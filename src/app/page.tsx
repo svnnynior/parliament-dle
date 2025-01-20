379e8ce8f3dc7975da3b74ddb9aba6a037a49761
svnnynior <pitchayut.jr@gmail.com>
2025-01-20 23:07:04 +0700

    feat: start layouting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,29 @@
+import QuoteText from "@/components/QuoteText";
 import Image from "next/image";
 
 export default function Home() {
+  const promiseTitle = "จัดตั้งกองทุนส่งเสริมดิจิทัล Start Up 5,000 ล้านบาท";
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="min-h-screen flex flex-col gap-8">
+      <header className="flex flex-row justify-between items-center p-4 border-b border-gray-200">
+        <span className="text-sm md:text-base">How-to</span>
+        <div className="flex flex-col items-center">
+          <span className="text-xl md:text-3xl font-bold">
+            Parliamentdle 🇹🇭
+          </span>
+          <span className="text-sm md:text-base">
+            Guess who made the promise
+          </span>
+        </div>
+        <span className="text-sm md:text-base">Stats</span>
+      </header>
+      <main className="flex flex-col px-12 w-full justify-center items-center">
+        <div className="flex flex-col gap-4 w-full md:w-2/3 lg:w-1/3">
+          <QuoteText text={promiseTitle} />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <footer className="absolute bottom-0 pb-4 md:pb-8 w-full flex items-center justify-center">
+        <span className="flex flex-rol">
           <Image
             aria-hidden
             src="https://nextjs.org/icons/globe.svg"
@@ -93,8 +31,16 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
-        </a>
+          &nbsp; Created using data from &nbsp;
+          <a
+            className="flex bold underline underline-offset-2 hover:underline-offset-4 -ml-1"
+            href="https://wevis.info/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            WeVIS
+          </a>
+        </span>
       </footer>
     </div>
   );
---
+++ src/components/QuoteText.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const QuoteText = ({ text }: { text: string }) => {
+  return (
+    <blockquote className="flex flex-col text-xl italic font-semibold text-white">
+      <div>
+        <svg
+          className="w-8 h-8 mb-2 -ml-6"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 290 290"
+          fill="#777"
+        >
+          <path d="M22.12 145v97.65h97.65V145H70.95c0-26.92 21.9-48.82 48.82-48.82V47.35c-53.93 0-97.65 43.72-97.65 97.65zm245.76-48.82V47.35c-53.93 0-97.65 43.72-97.65 97.65v97.65h97.65V145h-48.82c-.01-26.92 21.89-48.82 48.82-48.82z"></path>
+        </svg>
+      </div>
+      <div className="text-xl text-center">{text}</div>
+      <div className="self-end">
+        <svg
+          className="w-8 h-8 mt-2 -mr-6"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 290 290"
+          fill="#777"
+        >
+          <path d="M267.88 145V47.35h-97.65V145h48.82c0 26.92-21.9 48.82-48.82 48.82v48.82c53.93.01 97.65-43.71 97.65-97.64zM22.12 193.82v48.82c53.93 0 97.65-43.72 97.65-97.65V47.35H22.12V145h48.82c.01 26.92-21.89 48.82-48.82 48.82z"></path>
+        </svg>
+      </div>
+    </blockquote>
+  );
+};
+
+export default QuoteText;
